5819fae9c2891a4e0728c06649d3633be770ca18
GenadyKogan <rgkogan@gmail.com>
2022-11-14 16:12:38 +0200

    part 1

--- part1.py
+++ part1.py
@@ -17,14 +17,15 @@ import matplotlib.pyplot as plt
 
 
 class part1():
-    def __init__(self):
+    r=0
+    def __init__(self,T1=None,T2=None,T3=None):
         self.r=0
 
         # reliabilityNetwork = R
         self.R=-1
         self.G = nx.Graph()
 
-        # init Disjoint Set Structures according 4
+        # init Disjoint Set Structures according to step 4
         self.ds = DisjointSet()
 
         # M1 and M2 according D
@@ -35,21 +36,13 @@ class part1():
         self.pValues = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99]
         self.table = {}
         # get from teacher 
-        self.T1=0
-        self.T2=0
-        self.T3=0
+        self.T1=T1
+        self.T2=T2
+        self.T3=T3
 
-    def createrDSS(self):
-        for i in range(1, 21):
+    
             
-            # find() return the canonical element of a given item.
-            # In case the element was not present in the data structure,
-            # the canonical element is the item itself.
-            self.ds.find(i)
-
-    def calculateDSS(self):
-        if self.ds.connected(self.T1,self.T2) and self.ds.connected(self.T2,self.T3):
-            return True
+            
     
     # Help function for edge settings. accorsing to step 3
     def booleanValue(self,p,nodeU, nodeV):
@@ -58,7 +51,7 @@ class part1():
         # random.uniform(0, 1)
         # Return a random number between, and included, 0 and 1:
         value = random.uniform(0, 1)
-
+        
         # step 3.2
         # if p >= value graph edge in UP else Down
         # g --> up
@@ -72,32 +65,43 @@ class part1():
             return "g"
         return "r"
 
+    # Step A
     def makeGraph(self,p):
-        self.createrDSS()
 
+        # Disjoint Set Structures
+        self.ds=DisjointSet()
+        for i in range(1, 21):
+
+            # find() return the canonical element of a given item.
+            # In case the element was not present in the data structure,
+            # the canonical element is the item itself.
+            self.ds.find(i)
+
+        # Creating state vector 
         # Add a single node `node_for_adding` and update node attributes 
         # :first param: node for adding
         # :second param pos: posinton in type: tuple[float, float]     
-        self.G.add_node(1,pos=(6.123233995736766e-17, -1.0))
-        self.G.add_node(2, pos=(-0.9510565162951535, -0.3090169943749475))
-        self.G.add_node(3, pos=(-0.5877852522924732, 0.8090169943749473))
-        self.G.add_node(4, pos=(0.5877852522924731, 0.8090169943749475))
-        self.G.add_node(5, pos=(0.9510565162951535, -0.3090169943749474))
+        
+        self.G.add_node(1,pos=(0.002, -0.80))
+        self.G.add_node(2, pos=(-0.96, -0.30))
+        self.G.add_node(3, pos=(-0.59, 0.81))
+        self.G.add_node(4, pos=(0.59, 0.81))
+        self.G.add_node(5, pos=(0.95, -0.31))
         self.G.add_node(6, pos=(2.367, -0.75))
-        self.G.add_node(7, pos=(1.7633557568774194, -2.4270509831248424))
-        self.G.add_node(8, pos=(1.2246467991473532e-16, -2.5))
-        self.G.add_node(9, pos=(-1.7633557568774192, -2.4270509831248424))
+        self.G.add_node(7, pos=(1.76, -2.43))
+        self.G.add_node(8, pos=(0.00, -2.5))
+        self.G.add_node(9, pos=(-1.76, -2.43))
         self.G.add_node(10, pos=(-2.3, -0.78))
-        self.G.add_node(11, pos=(-2.853169548885461, 0.9270509831248419))
+        self.G.add_node(11, pos=(-2.85, 0.93))
         self.G.add_node(12, pos=(-1.461, 2.04))
-        self.G.add_node(13, pos=(-5.51091059616309e-16, 3.0))
+        self.G.add_node(13, pos=(0, 3.0))
         self.G.add_node(14, pos=(1.4, 1.99))
-        self.G.add_node(15, pos=(2.8531695488854605, 0.9270509831248421))
-        self.G.add_node(16, pos=(4.755282581475767, 1.545084971874737))
-        self.G.add_node(17, pos=(2.938926261462366, -4.045084971874737))
-        self.G.add_node(18, pos=(-2.938926261462365, -4.045084971874737))
-        self.G.add_node(19, pos=(-4.755282581475768, 1.5450849718747364))
-        self.G.add_node(20, pos=(-9.184850993605148e-16, 5.0))
+        self.G.add_node(15, pos=(2.85, 0.93))
+        self.G.add_node(16, pos=(4.76, 1.55))
+        self.G.add_node(17, pos=(2.94, -4.04))
+        self.G.add_node(18, pos=(-2.91, -4.04))
+        self.G.add_node(19, pos=(-4.76, 1.55))
+        self.G.add_node(20, pos=(0, 5.0))
 
         # Add an edge between u and v. The nodes u and v will be
         # automatically added if they are not already in the graph.
@@ -109,6 +113,7 @@ class part1():
         # attr : keyword arguments, optional
             # Edge data (or labels or objects) can be assigned using
             # keyword arguments.
+
         self.G.add_edge(1, 2, color=self.booleanValue(p, 1, 2), weight=1)
         self.G.add_edge(2, 3, color=self.booleanValue(p, 2, 3), weight=2)
         self.G.add_edge(3, 4, color=self.booleanValue(p, 3, 4), weight=3)
@@ -139,52 +144,127 @@ class part1():
         self.G.add_edge(3, 12, color=self.booleanValue(p, 3, 12), weight=28)
         self.G.add_edge(4, 14, color=self.booleanValue(p, 4, 14), weight=29)
         self.G.add_edge(5, 6, color=self.booleanValue(p, 5, 6), weight=30)
+        
+    def stateVector(self):
+        print("DSS")
+        print(list(self.ds.itersets()))
+        edges = list(self.G.edges())
+        stateVector = {}
+        for i in range(len(edges)):
+            stateVector[edges[i]] = [self.G[u][v]['color'] for u, v in self.G.edges()][i]
+        state = []
+        for k,v in stateVector.items():
+                state.append((k,v))
+        
+        # Printing stata vector depending on p value
+        print("State Vector")
+        print(state)
 
     # dodecahedron network representation
     def graphShow(self):
         self.makeGraph(0.5)
-        plt.subplots(1, 1, figsize=(14, 9))
+
+        # Create a figure and a set of subplots.
+        # This utility wrapper makes it convenient to create
+        # common layouts of subplots, including the enclosing
+        # figure object, in a single call.
+        plt.subplots(1, 1, figsize=(8, 8))
+
+        # Get node attributes from graph
         pos = nx.get_node_attributes(self.G, name='pos')
+
+        # Get edge attributes from graph
         labels = nx.get_edge_attributes(self.G, 'weight')
-        edges = self.G.edges()
-        colors = [self.G[u][v]['color'] for u, v in edges]
-        edges = list(edges)
-        stateVector = {}
-        for i in range(len(edges)):
-            stateVector[edges[i]] = colors[i]
-        state = []
-        for k,v in stateVector.items():
-                state.append((k,v))
-        print("State Vector")
-        print(state)
-        print("-----------------")
-        print("DSS")
-        print(list(self.ds.itersets()))
-        nx.draw(self.G, pos=pos, arrows=False, edge_color=colors, node_color='#7EC0EE', with_labels=True, width=1.5)
+
+        self.stateVector()
+   
+        # Draw the graph as a simple representation with no node
+        # labels or edge labels and using the full Matplotlib figure area
+        # and no axis labels by default.
+        nx.draw(self.G, pos=pos, arrows=False,
+                edge_color=[self.G[u][v]['color'] for u, v in self.G.edges()],
+                node_color='#9eee7e', with_labels=True, width=1.5)
+        
+        # Draw edge labels
         nx.draw_networkx_edge_labels(self.G, pos, edge_labels=labels)
+
+        # Return the figure manager of the current figure.
+        # The figure manager is a container for the actual
+        # backend-depended window that displays the figure
+        # on screen. If no current figure exists, a new one
+        # is created, and its figure manager is returned.
         plt.get_current_fig_manager().window.wm_geometry("+300+25")
         plt.show()
 
-    # Craeting table according D
+    
+
+    # Step 6 using M1
+    # Calculating reliability network
+    def calculateReliabilityNetworkM1(self):
+        self.R=self.r/self.M1
+        return self.R
+
+    # Step 6 using M2
+    # Calculating reliability network using M1 
+    def calculateReliabilityNetworkM2(self):
+        self.R=self.r/self.M2
+        return self.R
+
+    # Step D
+    # Craeting table 
     def createTable(self):
         p = 0.01
-        for i in range(99):
+        for _ in range(99):
             self.table[p] = [p, -1, -1]
             p += 0.01
-            p = "{:.2f}".format(float(p))
+            p = "{:.2f}".format(p)
             p = float(p)
-            
-    # Printing table according D
+    
+  
+    # Printing table 
     def printTable(self):
         print("Terminals:"+" T1=" + str(self.T1) + " T2=" + str(self.T2) + " T3=" + str(self.T3))
-        print("  P    M1   M2")
-        print(" -------------")
+        print("\n  P    M1   M2\n")
         for p in self.pValues:
             print(self.table[p])
     
+    def calculateDSS(self):
+        
+        # Return True if T1 and T2 and T3 belong to the same set.
+        return self.ds.connected(self.T1,self.T2) and self.ds.connected(self.T2,self.T3)
+
+    # Step C
+    # Calculating network reliability for p value
+    def assessNetworkReliability(self):
+        self.createTable()
+        for p in self.pValues:
+           
+            for _ in range(self.M1):
+                
+                # Function from according to A
+                self.makeGraph(p)
+
+                # Step 4
+                if True == self.calculateDSS(): self.r+=1
+            self.table[p][1] = self.calculateReliabilityNetworkM1()
+                   
+            for _ in range(self.M1,self.M2):
+                
+                # Function from according to A
+                self.makeGraph(p)
+
+                # Step 4
+                if True == self.calculateDSS(): self.r+=1
+            self.table[p][2] = self.calculateReliabilityNetworkM2()
+            self.r=0
+        self.printTable()
+       
+
 
 
 if __name__ == "__main__":
-    test=part1()
-    test.graphShow()
+    p=part1(0,0,0)
+    p.graphShow() 
+    p.assessNetworkReliability()
+    
     
