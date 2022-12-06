83ea7a2b2f5e6a0bf87e790a6a2e7e38ba86bceb
GenadyKogan <rgkogan@gmail.com>
2022-12-06 10:01:42 +0200

    add: relative error in part 2

--- part2_dir/part2.py
+++ part2_dir/part2.py
@@ -1,6 +1,22 @@
 from part1_dir.part1 import Part1
-
+import math
 class Part2(Part1):
     def __init__(self,x,v,c):
         super().__init__(x,v,c)
+
+    def relativeError(self,values):
+        sampleMean = 0
+        standardDeviation  = 0
+        sum = 0
+        for value in values:
+            sum += value
+        sampleMean = sum / len(values)
+        sum=0
+        for value in values:
+            power = math.pow((value - sampleMean),2)
+            sum += power        
+        standardDeviation = math.sqrt(sum/(len(values) - 1))
+        return standardDeviation/sampleMean
+
+    
        
